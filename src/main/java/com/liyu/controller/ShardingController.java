eb958ce116ec1f16559fd1dc5bc12f25547feb69
李煜 <liyu@100fen.com>
2019-07-02 15:38:23 +0800

    增加分库 分表相关配置
    Change-Id: Ie51a41e34ea2cc01c4418ef35c3927cdb6a3c701

--- src/main/java/com/liyu/controller/ShardingController.java
+++ src/main/java/com/liyu/controller/ShardingController.java
@@ -2,9 +2,6 @@ package com.liyu.controller;
 
 import com.liyu.dao.OrderMapper;
 import com.liyu.entity.Order;
-import org.apache.ibatis.executor.keygen.KeyGenerator;
-import org.apache.shardingsphere.core.strategy.keygen.SnowflakeShardingKeyGenerator;
-import org.apache.shardingsphere.spi.keygen.ShardingKeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +19,17 @@ public class ShardingController {
 
     @GetMapping("/v1/get")
     public void test() {
-        for(long i=0;i<100;i++) {
+        for (int i = 0; i < 100; i++) {
             Order order = new Order();
             order.setStatus("1");
-            order.setUserId(1);
-            orderMapper.insert(order);
+            order.setUserId(i);
+            orderMapper.addOrder(order);
         }
+    }
+
+    @GetMapping("/v1/get/v1")
+    public void test1() {
+        Order order = orderMapper.selectByPrimaryKey(1L);
+        System.out.println(order.getUserId());
     }
 }
